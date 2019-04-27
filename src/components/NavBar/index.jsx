7a22ecda8efbb14ff2ffa590ada05ae990452342
rob <robertchen234@gmail.com>
2019-04-26 22:19:40 -0400

    er now

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -30,7 +30,7 @@ class NavBar extends Component {
       <Navbar color="info" fixed="top" active={open}>
         <Navbar.Brand>
           <Navbar.Item>
-            <p>Blog Stacks</p>
+            <p>ER Now</p>
           </Navbar.Item>
 
           <Navbar.Burger onClick={this.toggleNavBar} />
@@ -40,7 +40,7 @@ class NavBar extends Component {
           <Navbar.Container position="end">
             {isUserSignedIn && (
               <React.Fragment>
-                <Navbar.Item>Posts</Navbar.Item>
+                <Navbar.Item>Messages</Navbar.Item>
                 <Navbar.Item>My Profile</Navbar.Item>
                 <Navbar.Item onClick={this.handleSignOut}>Sign Out</Navbar.Item>
               </React.Fragment>
