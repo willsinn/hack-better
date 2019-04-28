23c32d58bd9938498be6aa647b5de85945e29829
Dodie Matthews <dodiematthew@Dodies-MBP-15.local>
2019-04-28 05:12:36 -0400

    Input form css styled finished

--- src/pages/admin/_username/ProfileShowPage.jsx
+++ src/pages/admin/_username/ProfileShowPage.jsx
@@ -7,7 +7,7 @@ class ProfileShowPage extends Component {
     return (
       <div className="profile-container">
         <div className="profile-photo">
-          <img src={currentUser.photo_url} alt="profile" />
+          <img src="https://www.gooverseas.com/sites/default/files/styles/1014x/public/images/2018-03-11/Volunteer%20with%20Sea%20Turtles%20Hero.jpg?itok=6i_geFUq" alt="profile" />
         </div>
         <div className="profile-info">
           <h3>{currentUser.full_name}</h3>
