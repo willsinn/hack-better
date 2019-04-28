ac26b9a49d5f331e1fee4063c59c4280e03209c3
naomijoyce <naomijoyceb@gmail.com>
2019-04-28 06:35:12 -0400

    css changes for events

--- src/pages/EventShowPage.js
+++ src/pages/EventShowPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import IdeasContainer from "../components/Ideas/IdeasContainer";
+import TeamsContainer from "../components/Teams/TeamsContainer";
 
 class EventShowPage extends Component {
   state = {
@@ -13,8 +14,9 @@ class EventShowPage extends Component {
   render() {
     return (
       <div>
-        <h1> Pitch Ideas Voting </h1>
-        <h2> {this.props.event.title} </h2>
+        <h1 className="voting-title"> Idea Voting Phase </h1>
+        <h2 className="hack-event"> {this.props.event.title} </h2>
+        <div className="ideas-container" >
         {this.state.phase === 1 ? (
           <IdeasContainer
             createVote={this.props.createVote}
@@ -26,6 +28,7 @@ class EventShowPage extends Component {
             currentUser={this.props.currentUser}
           />
         )}
+      </div>
       </div>
     );
   }
