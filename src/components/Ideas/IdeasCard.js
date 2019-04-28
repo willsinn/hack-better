fd890c84b06b7e8675f3f71b830a4e280fe0d3cb
William Sinn <w.h.sinn@gmail.com>
2019-04-28 05:45:34 -0400

    commit

--- src/components/Ideas/IdeasCard.js
+++ src/components/Ideas/IdeasCard.js
@@ -1,38 +1,31 @@
 import React, { Component } from 'react';
 
 class IdeasCard extends Component {
-
-  state={
-    isClicked: false,
-    ideaVotes: this.props.idea.votes.length
-  }
-
-  handleClick = () => {
-    const {idea} = this.props
-    let ideaVotes = this.state.ideaVotes
-    if (!this.state.isClicked) {
-      this.props.createVote(idea.id) 
-      this.setState({isClicked: true, ideaVotes: ideaVotes+=1})}
-  }
-
   render() {
     const {idea} = this.props
-    
-    console.log(this.props.createVote)
+    console.log(idea);
+
     return (
-      <div>
-          <strong>{idea.title}</strong> 
-          <i>{idea.topic}</i>
-          <h5> by {idea.full_name}</h5>
+      <div className="idea-card-info">
+          <strong>{idea.title}</strong>
+          <h5>{idea.full_name}</h5>
 
-          
-          <p>Problem: {idea.problem}</p>
-          <p>Solution: {idea.solution}</p>
-          <i>Audience: {idea.audience}</i>
+          <p>Audience:</p>
+          <p>{idea.audience}</p>
 
-            <span onClick={this.handleClick}>❤️</span><span>{this.state.ideaVotes}</span>
-          <br/>
+          <p>Problem:</p>
+          <p>{idea.problem}</p>
+
+          <p>Solution:</p>
+          <p>{idea.solution}</p>
+
+          <div className="category-tag">
+            <div className="category-icon"></div>
+            <h3>{idea.topic}</h3>
+          </div>
+
       </div>
+
     );
   }
 }
--- src/pages/EventShowPage.js
+++ src/pages/EventShowPage.js
@@ -1,32 +1,18 @@
 import React, { Component } from "react";
 import IdeasContainer from "../components/Ideas/IdeasContainer";
-import TeamsContainer from "../components/Teams/TeamsContainer";
 
 class EventShowPage extends Component {
-  state = {
-    phase: 1
-  };
-
-  changePhase = () => {
-    this.setState({ phase: 2 });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1> Pitch Ideas Voting </h1>
-        <h2> {this.props.event.title} </h2>
-        {this.state.phase === 1 ? (
-          <IdeasContainer
-            createVote={this.props.createVote}
-            changePhase={this.changePhase}
-          />
-        ) : (
-          <TeamsContainer joinTeam={this.props.joinTeam} />
-        )}
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div>
+                <h1 className="voting-title"> Idea Voting Phase </h1>
+                <h2 className="hack-event"> {this.props.event.title} </h2>
+                <div className="ideas-container" >
+                  <IdeasContainer />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default EventShowPage;
